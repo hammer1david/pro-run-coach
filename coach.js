1715fe273747a9c895a8fc0be50350618fd5a2e7
hammer1david <hammer1.david@gmail.com>
2026-07-09 22:59:28 +0200

    coach.js

--- coach.js
+++ coach.js
@@ -26,3 +26,41 @@ function savePlan() {
     "Trainingsplan gespeichert ✅";
 
 }
+
+function loadProfile() {
+
+    let savedProfile = localStorage.getItem("strideLabProfile");
+
+    if (savedProfile) {
+
+        let profile = JSON.parse(savedProfile);
+
+        document.getElementById("athleteProfile").innerHTML = `
+
+        <div class="card">
+
+        <h3>Athletenprofil</h3>
+
+        <p>Name: ${profile.name}</p>
+
+        <p>Ziel: ${profile.goal}</p>
+
+        <p>Bestzeiten: ${profile.times}</p>
+
+        <p>Trainingstage pro Woche: ${profile.days}</p>
+
+        <p>Gewünschte Tage: ${profile.trainingDays.join(", ")}</p>
+
+        </div>
+
+        `;
+
+    } else {
+
+        document.getElementById("athleteProfile").innerHTML =
+        "Noch kein Profil gespeichert.";
+
+    }
+
+}
+
