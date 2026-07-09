bfee246283c00755e0266dd0fbc480ac67aa7c88
hammer1david <hammer1.david@gmail.com>
2026-07-09 22:32:54 +0200

    coach.js

--- coach.js
+++ coach.js
@@ -1,1 +1,28 @@
 
+function savePlan() {
+
+    let plan = {
+
+        monday: document.getElementById("monday").value,
+
+        tuesday: document.getElementById("tuesday").value,
+
+        wednesday: document.getElementById("wednesday").value,
+
+        thursday: document.getElementById("thursday").value,
+
+        saturday: document.getElementById("saturday").value
+
+    };
+
+
+    localStorage.setItem(
+        "strideLabPlan",
+        JSON.stringify(plan)
+    );
+
+
+    document.getElementById("message").innerHTML =
+    "Trainingsplan gespeichert ✅";
+
+}
