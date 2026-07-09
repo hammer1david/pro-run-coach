275bafc64744137db37191b9d7dc0c38db45b365
hammer1david <hammer1.david@gmail.com>
2026-07-09 22:24:40 +0200

    athlete.js

--- athlete.js
+++ athlete.js
@@ -1,1 +1,31 @@
 
+window.onload = function() {
+
+    let plan = `
+    Montag:
+    ${trainingPlan.monday}
+
+    Dienstag:
+    ${trainingPlan.tuesday}
+
+    Mittwoch:
+    ${trainingPlan.wednesday}
+
+    Donnerstag:
+    ${trainingPlan.thursday}
+
+    Freitag:
+    ${trainingPlan.friday}
+
+    Samstag:
+    ${trainingPlan.saturday}
+
+    Sonntag:
+    ${trainingPlan.sunday}
+    `;
+
+
+    document.getElementById("training").innerHTML =
+    plan.replace(/\n/g, "<br>");
+
+};
