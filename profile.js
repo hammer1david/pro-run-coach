0dc9e0e166afc29f6b11aece81f28719eb69bfaa
hammer1david <hammer1.david@gmail.com>
2026-07-09 22:52:10 +0200

    profile.js

--- profile.js
+++ profile.js
@@ -1,1 +1,38 @@
+function saveProfile() {
 
+    let trainingDays = [];
+
+    let checkboxes = document.querySelectorAll(
+        'input[type="checkbox"]:checked'
+    );
+
+    checkboxes.forEach(function(box) {
+        trainingDays.push(box.value);
+    });
+
+
+    let profile = {
+
+        name: document.getElementById("name").value,
+
+        goal: document.getElementById("goal").value,
+
+        times: document.getElementById("times").value,
+
+        days: document.getElementById("days").value,
+
+        trainingDays: trainingDays
+
+    };
+
+
+    localStorage.setItem(
+        "strideLabProfile",
+        JSON.stringify(profile)
+    );
+
+
+    document.getElementById("message").innerHTML =
+    "Profil gespeichert ✅";
+
+}
