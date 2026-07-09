14f328e9670f842e41d0ce4056861f658d0fe6ec
hammer1david <hammer1.david@gmail.com>
2026-07-09 22:22:20 +0200

    training.js

--- training.js
+++ training.js
@@ -1,1 +1,17 @@
+let trainingPlan = {
 
+    monday: "Locker Lauf + Mobilität",
+
+    tuesday: "Intervalle",
+
+    wednesday: "Regeneration",
+
+    thursday: "Tempolauf",
+
+    friday: "Ruhetag",
+
+    saturday: "Langer Lauf",
+
+    sunday: "Erholung"
+
+};
