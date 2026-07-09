65f498dd35817115ed805149843fd127ae19bce8
hammer1david <hammer1.david@gmail.com>
2026-07-09 23:53:16 +0200

    plan.js

--- plan.js
+++ plan.js
@@ -1,1 +1,25 @@
+function saveTrainingPlan() {
 
+    let plan = {
+
+        monday: document.getElementById("monday").value,
+        tuesday: document.getElementById("tuesday").value,
+        wednesday: document.getElementById("wednesday").value,
+        thursday: document.getElementById("thursday").value,
+        friday: document.getElementById("friday").value,
+        saturday: document.getElementById("saturday").value,
+        sunday: document.getElementById("sunday").value
+
+    };
+
+
+    localStorage.setItem(
+        "strideLabTrainingPlan",
+        JSON.stringify(plan)
+    );
+
+
+    document.getElementById("message").innerHTML =
+    "Trainingsplan gespeichert ✅";
+
+}
