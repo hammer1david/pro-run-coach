3ae301cdc9dde46a474aa9d829485f340efc2d18
hammer1david <hammer1.david@gmail.com>
2026-07-10 13:27:52 +0200

    Update training.js

--- training.js
+++ training.js
@@ -1,19 +1,29 @@
 window.onload = function(){
 
+    let url = window.location.href;
 
-let params = new URLSearchParams(window.location.search);
-
-
-let week = params.get("week");
+    console.log(url);
 
 
-if(week){
+    let params = new URLSearchParams(window.location.search);
 
-    document.getElementById("weekTitle").innerHTML =
-    "📅 Woche " + week;
+    let week = params.get("week");
 
-}
 
+    console.log("Woche:", week);
+
+
+    if(week){
+
+        document.getElementById("weekTitle").innerHTML =
+        "📅 Woche " + week;
+
+    } else {
+
+        document.getElementById("weekTitle").innerHTML =
+        "Keine Woche ausgewählt";
+
+    }
 
 };
 
