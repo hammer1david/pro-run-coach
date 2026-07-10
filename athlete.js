5dac56825c9131cb221d80c546e6e296826a77b9
hammer1david <hammer1.david@gmail.com>
2026-07-10 09:57:04 +0200

    Update athlete.js

--- athlete.js
+++ athlete.js
@@ -1,1 +1,39 @@
+function toggleProfile() {
 
+    let section = document.getElementById("profile");
+
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+
+}
+
+
+
+function toggleWeeks() {
+
+    let section = document.getElementById("weeks");
+
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+
+}
+
+
+
+function toggleFeedback() {
+
+    let section = document.getElementById("feedback");
+
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+
+}
